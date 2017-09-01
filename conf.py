96c5495c8a609015f3e9d11ec09091e43c0ed966
sdevriese <sdevriese@eos.ubc.ca>
2017-09-01 14:43:27 -0700

    small fix

--- conf.py
+++ conf.py
@@ -117,7 +117,7 @@ todo_include_todos = False
 
 # -- Edit on Github Extension ---------------------------------------------
 
-edit_on_github_project = 'ubcgif/GIFtoolsCookbook'
+edit_on_github_project = 'ubcgif/mag3d'
 edit_on_github_branch = 'master'
 check_meta = False
 
